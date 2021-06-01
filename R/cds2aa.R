8f4b058429f7db4b92b8a34a81e990ee2f1426e4
Kristian Ullrich <ullrich@evolbio.mpg.de>
2021-06-01 13:29:47 +0200

    #added shorten, frame and framelist to cds2aa

--- R/cds2aa.R
+++ R/cds2aa.R
@@ -2,6 +2,9 @@
 #' @name cds2aa
 #' @description This function translates a \code{DNAStringSet} into an \code{AAStringSet}.
 #' @param cds \code{DNAStringSet} [mandatory]
+#' @param shorten shorten all sequences to multiple of three [default: FALSE]
+#' @param frame  indicates the first base of a the first codon [default: 1]
+#' @param framelist  supply vector of frames for each entry [default: NULL]
 #' @return \code{AAStringSet}
 #' @importFrom Biostrings DNAString DNAStringSet AAString AAStringSet readDNAStringSet readAAStringSet writeXStringSet width subseq translate
 #' @importFrom stringr word
@@ -17,10 +20,23 @@
 #' @export cds2aa
 #' @author Kristian K Ullrich
 
-cds2aa <- function(cds){
+cds2aa <- function(cds, shorten = FALSE, frame = 1, framelist = NULL){
   if(class(cds)!="DNAStringSet"){stop("Error: input needs to be a DNAStringSet")}
+  if(!frame %in% c(1, 2, 3)){stop("Error: frame needs to be 1 or 2 or 3")}
+  if(!is.null(framelist)){
+    if(length(framelist) != length(cds)){stop("Error: framelist needs to be of equal length as cds")}
+  }
   if(!is.null(names(cds))){
     names(cds) <- stringr::word(names(cds), 1)
+  }
+  if(is.null(framelist)){
+    cds <- Biostrings::subseq(cds, frame, unique(Biostrings::width(cds)))
+  }
+  if(!is.null(framelist)){
+    cds <- Biostrings::subseq(cds, framelist, unique(Biostrings::width(cds)))
+  }
+  if(shorten){
+    cds <- Biostrings::subseq(cds, 1, Biostrings::width(cds) - Biostrings::width(cds) %% 3)
   }
   cds_not_multiple_of_three.idx <- which(Biostrings::width(cds) %% 3 != 0)
   if(length(cds_not_multiple_of_three.idx) > 0){
