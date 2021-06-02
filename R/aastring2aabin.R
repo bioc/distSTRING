bdbb790f65ba0299cef9774a2a9f7a29bf0eae82
Kristian Ullrich <ullrich@evolbio.mpg.de>
2021-06-02 18:06:03 +0200

    #added aastring2aabin which is just using ape::as.AAbin function

---
+++ R/aastring2aabin.R
@@ -0,0 +1,23 @@
+#' @title aastring2aabin
+#' @name aastring2aabin
+#' @description This function converts a \code{AAStringSet} into an \code{ape} \code{DNAbin}.
+#' @param dna \code{AAStringSet} [mandatory]
+#' @return An object of class \code{DNAbin}
+#' @importFrom Biostrings DNAString DNAStringSet AAString AAStringSet readDNAStringSet readAAStringSet writeXStringSet width subseq
+#' @importFrom ape as.AAbin
+#' @seealso \code{\link[seqinr]{as.alignment}} \code{\link[ape]{as.DNAbin.alignment}}
+#' @examples
+#' ## define two cds sequences
+#' cds1 <- Biostrings::DNAString("ATGCAACATTGC")
+#' cds2 <- Biostrings::DNAString("ATG---CATTGC")
+#' cds1.cds2.aln <- c(Biostrings::DNAStringSet(cds1),
+#'  Biostrings::DNAStringSet(cds2))
+#' ## convert into alignment
+#' aastring2aabin(cds2aa(cds1.cds2.aln))
+#' @export aastring2aabin
+#' @author Kristian K Ullrich
+
+aastring2aabin <- function(aa){
+  if(class(aa)!="AAStringSet"){stop("Error: input needs to be a AAStringSet")}
+  return(ape::as.AAbin(aa))
+}
